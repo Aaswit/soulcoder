715bf70bb9e6dd5dab6ec26e8c8ab7e179da9ebf
Aaswit <90980169+Aaswit@users.noreply.github.com>
2021-09-18 22:34:17 +0530

    Add files via upload
    Getting started with C++. Hope you like it and start a wonderful journey here with me and become a successful programmer/coder in near future. Thank you so much :)

---
+++ myprofile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    string name("Aaswit Kanhed");
+    char str[] = {"APSIT"};
+    int age(18);
+    char grade = 'A';
+    float cgpa = (9.5);
+    double rating = 8;
+    bool movie = true;
+
+    cout << "I am " << name << " and my age is " << age << ". I am a Second year Engineering Student at " << str << " and I passed my First year with " << grade << " Grade i.e with CGPA of " << cgpa << ". I also love watching movies but only if it has a rating above " << rating << endl;
+    return 0;
+}
