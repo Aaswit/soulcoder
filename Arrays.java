fd1efb71896356e948492155249f831308552969
Aaswit <90980169+Aaswit@users.noreply.github.com>
2022-02-07 17:12:16 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,15 @@
+    /*   syntax of arrays
+         datatype[] arrayName = new type[size];
+    i.e  int[] marks = new int[10];  or
+         int marks[] = new int[3];   or
+         int marks[] = {1,2,3,4,5,6}; 
+    indexing starts from 0 of arrays
+    */
+    public class Arrays {
+ 
+        public static void main(String args[]) {
+            int marks[] = {96,94,91};
+            System.out.println(marks[2]);
+        }
+
+}
